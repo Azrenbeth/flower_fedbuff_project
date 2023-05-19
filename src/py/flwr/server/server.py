64ed4d2fd5fda727feafd219b52139f8a338fe7c
Azrenbeth <77782548+Azrenbeth@users.noreply.github.com>
2023-05-19 09:44:40 +0100

    Add my name to copyright notices

--- src/py/flwr/server/server.py
+++ src/py/flwr/server/server.py
@@ -1,4 +1,5 @@
 # Copyright 2020 Adap GmbH. All Rights Reserved.
+# Copyright 2023 William Ashton
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
